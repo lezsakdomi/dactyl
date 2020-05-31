26219d3f954caf3a0ad1819cb44c866ed6d2e6b6
Domonkos Lezsák <lezsakdomi1@gmail.com>
2020-05-31 17:30:58 +0200

    Pin oak version to v5.0.0 and std version to 0.53.0

--- deps.ts
+++ deps.ts
@@ -6,6 +6,6 @@ export {
   RouterContext,
   Response,
   Middleware,
-} from "https://deno.land/x/oak/mod.ts";
-export { Status, STATUS_TEXT } from "https://deno.land/std/http/http_status.ts";
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+} from "https://deno.land/x/oak@v5.0.0/mod.ts";
+export { Status, STATUS_TEXT } from "https://deno.land/std@0.53.0/http/http_status.ts";
+export { assertEquals } from "https://deno.land/std@0.53.0/testing/asserts.ts";
